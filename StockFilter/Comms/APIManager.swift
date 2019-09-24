b17001e69750d091c884babd04ae0db0b9ff0d81
Mete Cakman <mete.cakman@gmail.com>
2019-09-24 16:22:02 +0200

    Created APIManager class and image decodable model.
    Renamed api endpoint to listImages and data type as "Image" rather than "Photo". Wrote mocked test and network integration test.

---
+++ StockFilter/Comms/APIManager.swift
@@ -0,0 +1,79 @@
+//
+//  APIManager.swift
+//  StockFilter
+//
+//  Created by Mete Cakman on 24/09/19.
+//  Copyright © 2019 Mete Cakman. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+
+
+/// Protocol for the basic API call that returns data, for dependency injection / mocking
+/// The intention is this data is what will be parsed by APIManager into useful Observables,
+/// So you can replace the APICaller and make sure you return some predictable Observable<Data> type
+/// Note that this is overkill for this simple app example, but demonstrates the principles
+/// of dependency injection and mocking unit tests.
+protocol APICaller {
+    func pullAPIData(from request: URLRequest) -> Observable<Data>
+}
+
+
+/// Simple manager class encapsulating our "API" endpoints, returning reactive sequences of data.
+/// At present there's just one GET endpoint (which actually points to static json data, but we don't necessarily know that on the client side.)
+class APIManager: NSObject {
+    
+    /// Dependency injected API caller
+    public var apiCaller: APICaller
+    
+    /// Swift errors are kind of annoying. Here's custom errors for our APIManager.. 
+    /// All this for just one error string.
+    enum APIManagerError: Error {
+        case badEndpointError
+        
+        var localizedDescription: String {
+            switch self {
+            case .badEndpointError:
+                return "Malformed url endpoint"
+            }
+        }
+    }
+    
+    /// Our default API caller implementation that works correctly using URLSession.shared.rx...
+    private class DefaultAPICaller: APICaller {
+        func pullAPIData(from request: URLRequest) -> Observable<Data> {
+                return URLSession.shared.rx
+                    .data(request: request)
+        }
+    }
+    
+    
+    // init with default apiCaller injected
+    init(apiCaller: APICaller = DefaultAPICaller()) {
+        self.apiCaller = apiCaller
+    }
+    
+    
+    
+    /// Hit our listImages endpoint, pulling the json data and returning an Observable sequence of Image arrays 
+    /// - Returns: Observable wrapped [Image] from server (note it just returns the one image array before completion)
+    public func listImages() -> Observable<[Image]> {
+        
+        let endPoint = "https://MeteC.github.io/StockFilter/server/api/listImages.json"
+        
+        guard let url = URL(string: endPoint) else {
+            print("Failed to create URL for end point \(endPoint)")
+            return Single<[Image]>
+                .error(APIManagerError.badEndpointError)
+                .asObservable()
+        }
+        
+        return self.apiCaller.pullAPIData(from: URLRequest(url: url))
+            .map({ (jsonData) -> [Image] in
+                // if this try throws an error it triggers an Rx error 
+                return try JSONDecoder().decode(ImageResponse.self, from: jsonData).images
+            })
+    }
+}
---
+++ StockFilterTests/CommsTests.swift
@@ -0,0 +1,99 @@
+//
+//  CommsTests.swift
+//  StockFilterTests
+//
+//  Created by Mete Cakman on 24/09/19.
+//  Copyright © 2019 Mete Cakman. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+@testable import StockFilter
+
+class CommsTests: XCTestCase {
+
+    let rxDisposeBag = DisposeBag()
+    
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+    
+    /// Test the API Manager listPhotos endpoint, mocking the json data provided to the APIManager class using dependency injection.
+    func testListPhotos() {
+        continueAfterFailure = false
+        
+        // Mock caller injection - inject expected API response into the APIManager rather than depend on the network for testing
+        class MockCaller: APICaller {
+            func pullAPIData(from request: URLRequest) -> Observable<Data> {
+                let returnJson = [
+                    "images": [[
+                        "id": 100,
+                        "thumb_url": "test_thumb_url",
+                        "url": "test_url",
+                        "title": "test_title",
+                        "author": "test_author",
+                        "updated": "2019-09-24 12:48:00"
+                    ]]
+                ]
+                let mockResultsData = try! JSONSerialization.data(withJSONObject: returnJson, options: [])
+                
+                return Single<Data>.create { (single) -> Disposable in
+                    single(.success(mockResultsData))
+                    return Disposables.create()
+                    }.asObservable()
+            }
+        }
+        
+        let apiMgr = APIManager()
+        apiMgr.apiCaller = MockCaller()
+        
+        let response = apiMgr.listImages()
+        let expect = expectation(description: "Wait for API")
+        
+        response.subscribe(onNext: { (images) in
+            XCTAssertEqual(images.count, 1)
+            XCTAssertEqual(images.first!.id, 100)
+            XCTAssertEqual(images.first!.thumbUrl, "test_thumb_url")
+            XCTAssertEqual(images.first!.url, "test_url")
+            XCTAssertEqual(images.first!.title, "test_title")
+            XCTAssertEqual(images.first!.author, "test_author")
+            XCTAssertEqual(images.first!.updated, "2019-09-24 12:48:00")
+            expect.fulfill()
+        }, onError: { (error) in
+            XCTFail("\(error)")
+        })
+        .disposed(by: rxDisposeBag)
+        
+        wait(for: [expect], timeout: 1.0)
+    }
+    
+    /// Integration test, pulling photo list from API.
+    /// As I control the static data the API provides, this is an easy test to make (I know what to expect.) 
+    /// In the normal case network integration tests can be more difficult to set up as future-proof tests!
+    func testIntegration_ListPhotos() {
+        continueAfterFailure = false
+        let apiMgr = APIManager()
+        
+        let response = apiMgr.listImages()
+        let expect = expectation(description: "Wait for API")
+        
+        response.subscribe(onNext: { (images) in
+            XCTAssertEqual(images.count, 1)
+            XCTAssertEqual(images.first!.id, 1)
+            XCTAssertEqual(images.first!.thumbUrl, "test_thumb_url")
+            XCTAssertEqual(images.first!.url, "test_url")
+            XCTAssertEqual(images.first!.title, "test_title")
+            XCTAssertEqual(images.first!.author, "test_author")
+            XCTAssertEqual(images.first!.updated, "2019-09-24 12:48:00")
+            expect.fulfill()
+        }, onError: { (error) in
+            XCTFail("\(error)")
+        })
+        .disposed(by: rxDisposeBag)
+        
+        wait(for: [expect], timeout: 10.0)
+    }
+
+}

5588725f81cfc33ff0b40de566e8bb3b0762211c
Mete Cakman <mete.cakman@gmail.com>
2019-09-24 12:31:54 +0200

    Initial Commit

---
+++ StockFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockFilter
+//
+//  Created by Mete Cakman on 24/09/19.
+//  Copyright © 2019 Mete Cakman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

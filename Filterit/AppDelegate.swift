bcca1f06b5df99cd8717f40d358e821cc8aefab0
Mete Cakman <mete.cakman@gmail.com>
2020-10-01 15:26:36 +0200

    Corrected highlighted tabs colour using UITabBar appearance proxy

--- Filterit/AppDelegate.swift
+++ Filterit/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Some global graphical traits
+        UITabBar.appearance().tintColor = UIColor(named: "Accent")
+        
         return true
     }
 
